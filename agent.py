fe45592856fe952decd199a5964ad8542e005174
asjlyle <a.sjlyle@gmail.com>
2018-06-18 18:28:20 +0100

    Update agent.py with epsilon greedy policy
    Update agent.py with epsilon greedy policy

--- agent.py
+++ agent.py
@@ -12,6 +12,8 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.alpha = 0.618
+        self.gamma = 1.0
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,11 +26,12 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        epsilon = 1.0/i_episode
+        
+        epsilon = 0.2
         policy_s=np.ones(self.nA)*epsilon/self.nA
         policy_s[np.argmax(self.Q[state])] = 1-epsilon + (epsilon / self.nA)
         
-        return np.random.choice(np.arange(self.nA,p=policy_s)
+        return np.random.choice(np.arange(self.nA),p=policy_s)
         #return np.random.choice(self.nA)
 
     def step(self, state, action, reward, next_state, done):
@@ -43,9 +46,11 @@ class Agent:
         - done: whether the episode is complete (True or False)
         """
         Qsa = self.Q[state][action]
+        #next_action = np.random.choice(self.nA)
+        next_action = self.select_action(next_state)
         Qsa_next = self.Q[next_state][next_action]
                                 
         if not done :
-             self.Q[state][action] = Qsa + (alpha * (reward +  Qsa_next) - Qsa)                   
+             self.Q[state][action] = Qsa + (self.alpha*(reward +  (self.gamma* Qsa_next)- Qsa))                   
         if done :
-             self.Q[state][action] = Qsa + (alpha * (reward - Qsa))
+             self.Q[state][action] = Qsa + (self.alpha* (reward - (self.gamma*Qsa)))
